25cd4da84f769948b27d4d600d18381ee3dc3566
rubicon2 <41235563+rubicon2@users.noreply.github.com>
2024-08-27 12:46:01 +0100

    Run project files through prettier

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 function lerp(min, max, t) {
   return min + (max - min) * t;
-};
+}
 
 function inverseLerp(min, max, value) {
   return (value - min) / (max - min);
-};
+}
 
 function loopNumber(min, max, value) {
   // "Normalize" position for lerp so it is always between min and max,
@@ -12,13 +12,13 @@ function loopNumber(min, max, value) {
   let t = inverseLerp(min, max, value);
   t = (1 + (t % 1)) % 1;
   return lerp(min, max, t);
-};
+}
 
-function loopInt (min, max, value) {
+function loopInt(min, max, value) {
   return Math.round(loopNumber(min, max, value));
-};
+}
 
 module.exports = {
   loopNumber,
-  loopInt
-}
+  loopInt,
+};
