795f1af9c3c4e5c3eab11a83f5082ce62e0c80e2
rubicon2 <41235563+rubicon2@users.noreply.github.com>
2026-02-03 11:32:12 +0000

    Add jsdoc comments to automatically create declaration file

--- index.js
+++ index.js
@@ -1,11 +1,27 @@
+/**
+ * @param {number} max
+ * @param {number} min
+ * @param {number} t
+ */
 function lerp(min, max, t) {
   return min + (max - min) * t;
 }
 
+/**
+ * @param {number} max
+ * @param {number} min
+ * @param {number} value
+ */
 function inverseLerp(min, max, value) {
   return (value - min) / (max - min);
 }
 
+/**
+ * @param {number} min
+ * @param {number} max
+ * @param {number} value
+ * @returns {number} The value looped over the min/max range. E.g. with a min of -1 and a max of 1, and a value of 1.5, this will return -0.5.
+ */
 function loopNumber(min, max, value) {
   // "Normalize" position for lerp so it is always between min and max,
   // and loops around by an amount proportional to how far it was beyond the range of 0 to 1.
@@ -14,6 +30,12 @@ function loopNumber(min, max, value) {
   return lerp(min, max, t);
 }
 
+/**
+ * @param {number} min
+ * @param {number} max
+ * @param {number} value
+ * @returns {number} The value looped over the min/max range and returned as an integer. Ideal for repeatedly looping through arrays.
+ */
 function loopInt(min, max, value) {
   // Must round value before looping, otherwise values very near to max will
   // not get looped round to the min but then get rounded up to a number outside the range.
